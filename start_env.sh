7dc3fd2c485e461af0e75e0374bfcf3773ed35c2
Arulraj <arulrajgopal@outlook.com>
2025-05-08 19:08:11 +0000

    shell script updated

--- start_env.sh
+++ start_env.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# Check if Python is installed
+if ! command -v python3 &>/dev/null; then
+    echo "Python is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y python3
+else
+    echo "Python is already installed."
+
+
 cd
 source /home/Arulraj/myenv/bin/activate
 cd Desktop/data_pipeline
 
+
+fi
