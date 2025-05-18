c4c5b4a33bc126f6054beb21825ecc6601f835ea
Your Name <you@example.com>
2025-05-18 18:20:36 +0000

    shell script update

--- start_env.sh
+++ start_env.sh
@@ -13,17 +13,17 @@ echo "Virtual environment directory: $VENV_DIR"
 # Check Python version
 python3 --version
 
-# # Install pip if missing
-# if ! command -v pip3 &> /dev/null
-# then
-#     echo "pip3 not found, installing..."
-#     sudo apt install -y python3-pip
-# else
-#     echo "pip3 is already installed."
-# fi
+# Install pip if missing
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 not found, installing..."
+    sudo apt install -y python3-pip
+else
+    echo "pip3 is already installed."
+fi
 
-# # Install venv if missing
-# sudo apt install -y python3-venv
+# Install venv if missing
+sudo apt install -y python3-venv
 
 # # Create virtual environment inside project directory if it doesn't exist
 # if [ ! -d "$VENV_DIR" ]; then
