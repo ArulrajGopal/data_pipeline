a620963aed4c954f53aa4947af30024d6acb78f3
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2025-06-19 23:38:51 +0530

    Update activate_venv.sh

--- activate_venv.sh
+++ activate_venv.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+cd
+VENV_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$PROJECT_DIR"
+
+
+echo "Project directory: $PROJECT_DIR"
+echo "Virtual environment directory: $VENV_DIR"
