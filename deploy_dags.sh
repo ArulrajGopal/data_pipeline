5b342e84fbfe0a14e5636f6140522a9b64f81ba6
Arulraj Gopal <arulrajgopal@outlook.com>
2025-06-20 16:26:20 +0000

    deploy dag updated

--- deploy_dags.sh
+++ deploy_dags.sh
@@ -9,18 +9,13 @@ cd "$PROJECT_DIR"
 echo "Project directory: $PROJECT_DIR"
 echo "Virtual environment directory: $VENV_DIR"
 
-
-
-SOURCE_DIR="$PROJECT_DIR/dags/"
-DEST_DIR="$VENV_DIR/airflow/dags"
-
 # Create destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 
 # Copy all files (not directories) from source to destination
-cp -a "$SOURCE_DIR"/* "$DEST_DIR"/
+cp -a "$PROJECT_DIR/dags/"/* "$VENV_DIR/airflow/dags"/
 
-echo "echo Files copied successfully from $SOURCE_DIR to $DEST_DIR"
+echo "dag files copied successfully from $ROJECT_DIR/dags/ to $VENV_DIR/airflow/dags"
 
 
 DAGS_DIR="$VENV_DIR/airflow/dags"
